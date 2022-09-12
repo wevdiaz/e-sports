36ff4c2a4a548763a43cf44481b22b4123d75339
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-12 17:15:23 -0300

    init no projeto, configuração de ambiente

---
+++ server/src/server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+  return res.json([
+    { id: 1, name: "Cereja" },
+    { id: 2, name: "Maracujá" },
+    { id: 3, name: "Pêssego" },
+    { id: 3, name: "Laranja" }
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("server is running!");
+});
