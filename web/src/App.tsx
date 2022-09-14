07fd87ba9637ae5acd3de0179ea8652c7fa8fa0c
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-13 22:53:24 -0300

    instalando e configurando o tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,12 @@
 
-interface ButtonProps {
-  title: String;
-}
+import "./styles/main.css";
 
-
-function Button(props : ButtonProps) {
-  return (
-    <button>{props.title}</button>
-  );
-}
 
 function App() {
 
   return (
-    <div>
-      <p>Nlw : E-Sports - 2022</p>
-      <div>
-        <Button title="Enviar 01" />
-        <Button  title="Enviar 02"/>
-        <Button title="Enviar 03" />
-      </div>      
+    <div className="">
     </div>
-
   )
 }
 
