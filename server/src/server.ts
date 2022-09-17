2756581a0374d6f127c9f9413ff8317db11266b8
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-17 17:47:18 -0300

    preparando as rotas da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,32 @@ import express from "express";
 
 const app = express();
 
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json(["Post!"]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  const { id } = req.params;
+  const frutas = [
+    { id: 1, name: "Cereja" },
+    { id: 2, name: "Maracujá" },
+    { id: 3, name: "Pêssego" },
+    { id: 3, name: "Laranja" }
+  ];
+
+  const fruta = frutas.find((fruta) => fruta.id  === Number(id));
+
+  if (!fruta) {
+    return console.log("Registro não encontrado!");    
+  }
+
+  return res.json([fruta]);
+});
+
 app.get("/ads", (req, res) => {
   return res.json([
     { id: 1, name: "Cereja" },
@@ -9,6 +35,10 @@ app.get("/ads", (req, res) => {
     { id: 3, name: "Pêssego" },
     { id: 3, name: "Laranja" }
   ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json(["discord"])
 });
 
 app.listen(3000, () => {
