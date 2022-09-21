b4cd9ef47bf1d255f49ff236b8521b442a23aa54
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-20 21:32:22 -0300

    Pegando os games sdo server via API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
@@ -5,7 +6,25 @@ import logoImg from "./assets/logoe-sports.svg";
 
 import "./styles/main.css";
 
+interface Games {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  const [games, setGames] = useState<Games[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/games")
+    .then(response => response.json())
+    .then((data) => {
+      setGames(data);
+    })
+  }, []);
 
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -15,7 +34,18 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="/game-1.png" title="League of legends" adsCount={3} />
+        {/* <GameBanner bannerUrl="/game-1.png" title="League of legends" adsCount={3} /> */}
+
+        {
+         games.length > 0 && games.map(game => (
+          <GameBanner 
+              key={game.id} 
+              bannerUrl={game.bannerUrl} 
+              title={game.title} 
+              adsCount={game._count.ads} 
+          />
+         ))
+        }
       </div>
 
       <CreateAdBanner />
